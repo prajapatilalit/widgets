d515d7d4fb19d3cd8a25f2d7f010f57ad143210f
prajapatilalit <prajapatilalit150992@gmail.com>
2021-09-02 19:06:18 +0530

    update fetch wikipedia api

--- widget/src/components/Search.js
+++ widget/src/components/Search.js
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Search = () => {
   const [text, setText] = useState("");
-  console.log("outside ");
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    console.log("inside use Effect");
+    const searchTerm = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: text,
+        },
+      });
+      setResults(data.query.search);
+    };
+    if (text) {
+      searchTerm();
+    }
   }, [text]);
+
   return (
     <div>
       <div className="ui form">
-        <input
-          type="text"
-          className="input"
-          placeholder="Search term..."
-          onChange={(e) => setText(e.target.value)}
-          value={text}
-        />
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            type="text"
+            className="input"
+            placeholder="Search term..."
+            onChange={(e) => setText(e.target.value)}
+            value={text}
+          />
+        </div>
       </div>
     </div>
   );
