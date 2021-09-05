70e307698353453cb38231b8fb24302c3fee3a21
prajapatilalit <prajapatilalit150992@gmail.com>
2021-09-06 01:20:02 +0530

    update conver component and translate google api

--- widget/src/components/Convert.js
+++ widget/src/components/Convert.js
@@ -1,12 +1,32 @@
 //https://translation.googleapis.com/language/translate/v2
 //AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState("");
   useEffect(() => {
-    console.log("new language");
+    const translationData = async () => {
+      const { data } = await axios.post(
+        "https://translation.googleapis.com/language/translate/v2",
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    translationData();
   }, [language, text]);
-  return <div />;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
